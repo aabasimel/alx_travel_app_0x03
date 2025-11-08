fb304b91ca1ced141642f61c0ace650d3f386d4c
Ahmed Abasimel <abasimel@Ahmeds-MacBook-Air.local>
2025-11-08 18:57:52 +0100

    chore: update URL configuration
    - Configure API documentation endpoints
    - Clean up URL routing structure
    - Prepare for API client integration

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -17,22 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="ALX Travel API",
-      default_version='v1',
-      description="API documentation for ALX Travel App",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
 )
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('listings/', include('listings.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', include('listings.urls')),  
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-

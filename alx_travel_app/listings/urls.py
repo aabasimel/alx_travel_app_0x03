6739fe6e802d504a708c1942307eca3242701db2
Ahmed Abasimel <abasimel@Ahmeds-MacBook-Air.local>
2025-11-09 13:56:19 +0100

    Add PaymentViewSet to url

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -3,13 +3,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views  
-from .views import PropertyViewSet, BookingViewSet, UserViewSet, ReviewViewSet,ApproveAdminView
+from .views import PropertyViewSet, BookingViewSet, UserViewSet, ReviewViewSet,ApproveAdminView,PaymentViewSet
 
 router = DefaultRouter()
 router.register(r"properties", PropertyViewSet, basename="property")
 router.register(r"bookings", BookingViewSet, basename="booking")
 router.register(r"reviews", ReviewViewSet, basename="review")
 router.register(r"users", UserViewSet, basename="user")
+router.register(r"payments", PaymentViewSet, basename="payment")
 
 APP_NAME = "listings"
 
